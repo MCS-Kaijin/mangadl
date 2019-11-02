0891de547b3c1d02abac73b677c485226c560bc8
Cassandra Gentry <cass@cassarch.localdomain>
2019-11-02 17:14:05 -0500

    Hunted down some bugs

--- mangadl.py
+++ mangadl.py
@@ -1,29 +1,35 @@
-import sqlite3, requests, bs4, os
+import requests, bs4, os, ui
 
 
-title = input('Name of manga: ').replace(' ', '_').lower()
-chaprange = input('From chapter-To chapter: ')
-frm, t = chaprange.split('-')
-frm, t = int(frm), int(t)
-url = 'https://mangakakalot.com/manga/' + title
-source = requests.get(url).content
-soup = bs4.BeautifulSoup(source, 'html.parser')
-a = soup.find_all('a', href=True)
-chapter_list = []
-for href in a:
-    if url.replace('/manga/', '/chapter/') in href['href']:
-        chapter_list.append(href['href'])
-chapter_list.reverse()
-i = frm
-while i <= t:
-    chapter = chapter_list[i]
-    os.mkdir('chapter{}'.format(str(i)))
-    csource = requests.get(chapter).content
-    csoup = bs4.BeautifulSoup(csource, 'html.parser')
-    imgs = csoup.find_all('img', src=True)
-    for l in range(len(imgs)):
-        img = imgs[l]
-        if '/mangakakalot/' in img['src']:
-            with open('page{}.jpg'.format(l), 'rb') as f:
-                f.write(requests.get(img['src']).content)
-    i += 1
+def get_manga(title, frm, t):
+    if not os.path.exists(title):
+        os.mkdir(title)
+    os.chdir(title)
+    search_url = 'https://mangakakalot.com/search/' + title
+    search_source = requests.get(search_url).content
+    search_soup = bs4.BeautifulSoup(search_source, 'html.parser')
+    results = search_soup.find_all('div')
+    url = [result.a['href'] for result in results if result.has_attr('class') and 'story_item' in result['class']][0]
+    #url = 'https://mangakakalot.com/manga/' + title
+    source = requests.get(url).content
+    soup = bs4.BeautifulSoup(source, 'html.parser')
+    a = soup.find_all('a', href=True)
+    chapter_list = []
+    for href in a:
+       if url.replace('/manga/', '/chapter/') in href['href']:
+            chapter_list.append(href['href'])
+    chapter_list.reverse()
+    i = frm
+    while i <= t:
+        chapter = chapter_list[i]
+        os.mkdir('chapter{}'.format(str(i)))
+        os.chdir('chapter{}'.format(str(i)))
+        csource = requests.get(chapter).content
+        csoup = bs4.BeautifulSoup(csource, 'html.parser')
+        imgs = csoup.find_all('img', src=True)
+        for l in range(len(imgs)):
+            img = imgs[l]
+            if '/mangakakalot/' in img['src']:
+                with open('page{}.jpg'.format(l), 'wb') as f:
+                    f.write(requests.get(img['src']).content)
+        i += 1
